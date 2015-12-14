6662f1fe83469b32cbb45e19f38119045f0a0af1
evgar <eugene.garichev@gmail.com>
2015-12-14 13:01:09 +0200

    add represent model

---
+++ battleship.js
@@ -0,0 +1,26 @@
+var view = {
+  displayMessage: function(msg) {
+  	// метод для вывода действий пользователя
+    var messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML = msg;
+  },
+  displayHit: function(location) {
+  	// метод для вывода изображения в соответствующей клетке при попадании
+  	var cell = document.getElementById(location);
+	cell.setAttribute("class", "hit");
+  },
+  displayMiss: function(location) {
+  	// метод для вывода изображения в соответствующей клетке при промахе
+  	var cell = document.getElementById(location);
+	cell.setAttribute("class", "miss");
+  }
+};
+
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+
+view.displayMessage("Hello!");
